e732ccd4ef55d4f703e294922e5818e56667a0a1
Galborin <piotrkoziar@hotmail.com>
2019-03-14 17:10:17 +0100

    removed waveplayer

---
+++ inc/record.h
@@ -0,0 +1,18 @@
+/*
+ * record.h
+ *
+ *  Created on: 11.01.2019
+ *      Author: piotr
+ */
+
+#ifndef __RECORD_H
+#define __RECORD_H
+
+// Includes
+#include "main.h"
+
+// Exported functions
+AUDIO_ErrorTypeDef AUDIO_REC_Process(void);
+AUDIO_ErrorTypeDef AUDIO_REC_Start(void);
+
+#endif /* __RECORD_H */
